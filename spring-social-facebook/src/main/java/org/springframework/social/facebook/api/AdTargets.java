05ac814fa10126c7346872319e9ce8c1d94d2f86
Eric Mayhew <eric.mayhew@dealer.com>
2011-09-09 10:18:06 -0400

    AdTargets, still getting used to Eclipse not auto-saving

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/api/AdTargets.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/AdTargets.java
@@ -2,42 +2,6 @@ package org.springframework.social.facebook.api;
 
 import java.util.List;
 
-
-//Name	Type	Description
-//countries	array	The countries parameter is the only required targeting parameter. It must be an array of country names. To use the complete list, incorporate the Autocomplete file (referenced above) into your app. Limit: 25 countries.
-//cities	array	Use the complete cities list by incorporating the Autocomplete file (referenced above) into your app. For example, specify "id":"2421215","name":"Palo Alto, CA". Alternatively, for cities in the US or Canada, specify the two letter state or province code, e.g. "name":"New York,NY" (or for international cities, the country name, e.g. "name":"Paris,France"). Limit: 200 cities.
-//zips	array	String array of 5-digit ZIP codes (postal codes) specified by the United States Postal Service. For example: '94304'. Currently, the zips parameter does not support international postal codes, autocomplete, the radius parameter, or a countries parameter specification outside of 'US'. Limit: 2500 zip codes.
-//regions	array	State, province, or region. To use the complete list, incorporate the Autocomplete file (referenced above) into your app. Limit: 200 regions.
-//radius	int	Must be used in conjunction with cities. Specifies a radius around cities.
-//locales	array	Array of locales, specified as indices. The indices are in a sub-array called 'locales'. To use the complete list, incorporate the Autocomplete file (referenced above) into your app. Use of this parameter includes for targeting users with a different language than the common language for the specified location parameter; you specify an ID for the language, e.g. 5 for German. Limit: 50 locales.
-//
-//
-//		keywords	array	Array of keyword strings for precise interest targeting. The keywords parameter applies only to precise interest targeting. See topic targeting. Keywords (either topic keywords or non-topic keywords) are matched to data from users' profiles. For example, if an advertiser sells cameras, targeting parameters could specify users who are interested in photography. You can post targeting data to /act_{account-id}/reachestimate to determine if proposed keywords match any profiles. Limit: 200 keywords.
-//user_adclusters	array	Array of ID-name pairs for broad-category targeting (BCT). The user_adclusters parameter applies only for BCT, which cannot be used with precise interest targeting. To include BCT in an ad, use act_{account-id}/broadtargetingcategories. For an example of how to use this parameter, see broad category targeting, above. Limit: 50 ID-name pairs.
-//interested_in	array	Indicates targeting based on the "interested in" field of the user profile. You can specify an integer of 1 to indicate MALE (for example, if you specify "interested_in":[1]). An integer of 2 indicates FEMALE. The default of ALL is used if you specify Null. The default of ALL is also used if you do not specify the field. Do not specify zero.
-//Demographics and events
-//Name	Type	Description
-//genders	array	Indicates gender-based targeting. You can specify an integer of 1 to indicate MALE (for example, if you specify "genders":[1]). An integer of 2 indicates FEMALE. The default of ALL is used if you specify Null. The default of ALL is also used if you do not specify the field. Do not specify zero.
-//age_min	int	Minimum age. If used, must be 13 or higher.
-//age_max	int	Maximum age.
-//broad_age	int	Whether or not to enable "broad-age-match" targeting. The default is 0 (disabled). Set to 1 to enable broad-age-match targeting.
-//relationship_statuses	array	Array of integers for targeting based on relationship status. Use 1 for SINGLE, 2 for IN_RELATIONSHIP, 3 for MARRIED, and 4 for ENGAGED. The default of ALL is used if you specify Null. The default of ALL is also used if you do not specify the field. Do not specify zero.
-//user_event	array	Array of integers corresponding to user-relevant events. Currently only supports birthday. To use birthday, pass a one-member array with a value of 1, e.g. "user_event":[1].
-//Facebook connections
-//Name	Type	Description
-//connections	array	An array of Facebook IDs. Connections allows you to target your ads to users who have become a fan of your Page, a member of your Group, RSVPÕd to your Event or authorized your Application. The session user has to be an admin of the page, group or event, or developer of the application that is being targeted against. Limit: 50 connections.
-//excluded_connections	array	An array of Facebook IDs. Excluded connections allows you to target your ads to users who have not become fans of your Page, members of your Group, RSVP'd to your Event or authorized your Application. The session user has to be an admin of the page, group or event, or developer of the application that is being targeted against.
-//friends_of_connections	array	An array of Facebook IDs. "Friends of connections" allows you to target friends of your connections. Connections are fans of your Page, users who have RSVPÕd Yes or Maybe to your Event, members of your Group, and users who have interacted with your app. The session user has to be an admin of the page, group or event, or a developer of the application that is being targeted against.
-//Education and work
-//Note that for the complete list of work networks, and of college networks and majors, the Autocomplete file (referenced above) should be downloaded and incorporated into your app.
-//
-//Name	Type	Description
-//college_networks	array	Colleges, for college graduates. Limit: 200 college networks.
-//work_networks	array	Company, organization, or other workplace. Limit: 200 work networks.
-//education_statuses	array	Array of integers for targeting based on education level. Use 1 for HIGH_SCHOOL, 2 for UNDERGRAD, and 3 for ALUM. For one ad group, you can specify only one value. The education_statuses parameter is required if you specify the college_majors parameter.
-//college_years	array	Array of integers for graduation year from college.
-//college_majors	array	Array of strings for specific college majors. Requires that you also specify a value for the education_statuses parameter. Limit: 200 college majors.
-
 public class AdTargets {
 
 	// Location Targets
@@ -45,14 +9,258 @@ public class AdTargets {
 	private List<String> cities;
 	private List<String> zips;
 	private List<String> regions;
-	private int radius;
+	private Integer radius;
 	private List<String> locales;
 	
-	// Likes / Interest Targets
-	
+	// Likes / Interest Targets	
 	private List<String> keywords;
-	
+	private List<UserAdCluster> userAdClusters;	
 	private List<Integer> interestedIn;
 	
+	// Demographics / Events
+	private List<Gender> genders;
+	private Integer ageMin;
+	private Integer ageMax;
+	private BroadAge broadAge;
+	private RelationshipStatus relationshipStatus;
+	private UserEvent userEvent;
 	
+	// Facebook Connections
+	private List<String> connections;
+	private List<String> excludedConnections;
+	private List<String> friendsOfConnections;
+	
+	// Education and Work
+	private List<String> collegeNetworks;
+	private List<String> workNetworks;
+	private List<EducationStatus> educationStatuses;
+	private List<Integer> collegeYears;
+	private List<String> collegeMajors;
+	
+	public void setGenders( List<Gender> genders ) {
+		this.genders = genders;
+	}
+	
+	public List<Gender> getGenders( ) {
+		return genders;
+	}
+
+	public List<String> getCountries() {
+		return countries;
+	}
+
+	public void setCountries(List<String> countries) {
+		this.countries = countries;
+	}
+
+	public List<String> getCities() {
+		return cities;
+	}
+
+	public void setCities(List<String> cities) {
+		this.cities = cities;
+	}
+
+	public List<String> getZips() {
+		return zips;
+	}
+
+	public void setZips(List<String> zips) {
+		this.zips = zips;
+	}
+
+	public List<String> getRegions() {
+		return regions;
+	}
+
+	public void setRegions(List<String> regions) {
+		this.regions = regions;
+	}
+
+	public Integer getRadius() {
+		return radius;
+	}
+
+	public void setRadius(Integer radius) {
+		this.radius = radius;
+	}
+
+	public List<String> getLocales() {
+		return locales;
+	}
+
+	public void setLocales(List<String> locales) {
+		this.locales = locales;
+	}
+
+	public List<String> getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(List<String> keywords) {
+		this.keywords = keywords;
+	}
+
+	public List<UserAdCluster> getUserAdClusters() {
+		return userAdClusters;
+	}
+
+	public void setUserAdClusters(List<UserAdCluster> userAdClusters) {
+		this.userAdClusters = userAdClusters;
+	}
+
+	public List<Integer> getInterestedIn() {
+		return interestedIn;
+	}
+
+	public void setInterestedIn(List<Integer> interestedIn) {
+		this.interestedIn = interestedIn;
+	}
+
+	public Integer getAgeMin() {
+		return ageMin;
+	}
+
+	public void setAgeMin(Integer ageMin) {
+		this.ageMin = ageMin;
+	}
+
+	public Integer getAgeMax() {
+		return ageMax;
+	}
+
+	public void setAgeMax(Integer ageMax) {
+		this.ageMax = ageMax;
+	}
+
+	public BroadAge getBroadAge() {
+		return broadAge;
+	}
+
+	public void setBroadAge(BroadAge broadAge) {
+		this.broadAge = broadAge;
+	}
+
+	public RelationshipStatus getRelationshipStatus() {
+		return relationshipStatus;
+	}
+
+	public void setRelationshipStatus(RelationshipStatus relationshipStatus) {
+		this.relationshipStatus = relationshipStatus;
+	}
+
+	public UserEvent getUserEvent() {
+		return userEvent;
+	}
+
+	public void setUserEvent(UserEvent userEvent) {
+		this.userEvent = userEvent;
+	}
+
+	public List<String> getConnections() {
+		return connections;
+	}
+
+	public void setConnections(List<String> connections) {
+		this.connections = connections;
+	}
+
+	public List<String> getExcludedConnections() {
+		return excludedConnections;
+	}
+
+	public void setExcludedConnections(List<String> excludedConnections) {
+		this.excludedConnections = excludedConnections;
+	}
+
+	public List<String> getFriendsOfConnections() {
+		return friendsOfConnections;
+	}
+
+	public void setFriendsOfConnections(List<String> friendsOfConnections) {
+		this.friendsOfConnections = friendsOfConnections;
+	}
+
+	public List<String> getCollegeNetworks() {
+		return collegeNetworks;
+	}
+
+	public void setCollegeNetworks(List<String> collegeNetworks) {
+		this.collegeNetworks = collegeNetworks;
+	}
+
+	public List<String> getWorkNetworks() {
+		return workNetworks;
+	}
+
+	public void setWorkNetworks(List<String> workNetworks) {
+		this.workNetworks = workNetworks;
+	}
+
+	public List<EducationStatus> getEducationStatuses() {
+		return educationStatuses;
+	}
+
+	public void setEducationStatuses(List<EducationStatus> educationStatuses) {
+		this.educationStatuses = educationStatuses;
+	}
+
+	public List<Integer> getCollegeYears() {
+		return collegeYears;
+	}
+
+	public void setCollegeYears(List<Integer> collegeYears) {
+		this.collegeYears = collegeYears;
+	}
+
+	public List<String> getCollegeMajors() {
+		return collegeMajors;
+	}
+
+	public void setCollegeMajors(List<String> collegeMajors) {
+		this.collegeMajors = collegeMajors;
+	}
+
+	
+	public enum Gender {
+		MALE(1), FEMALE(2), ALL(null);
+		private Integer gender;
+		private Gender(Integer gender){ this.gender = gender; }
+		public Integer getGender() { return gender; }
+		public void setGender(Integer gender) { this.gender = gender; }
+	}
+
+	public enum BroadAge {
+		DISABLED(0), ENABLED(1);
+		private Integer broadAge;
+		private BroadAge(Integer broadAge){ this.broadAge = broadAge; }
+		public Integer getBroadAge() { return broadAge; }
+		public void setBroadAge(Integer broadAge) { this.broadAge = broadAge; }
+	}
+	
+	public enum RelationshipStatus {
+		SINGLE(1), IN_RELATIONSHIP(2), MARRIED(3), ENGAGED(4), ALL(null);
+		private Integer relationshipStatus;
+		private RelationshipStatus(Integer relationshipStatus){ this.relationshipStatus = relationshipStatus; }
+		public Integer getRelationshipStatus() { return relationshipStatus; }
+		public void setRelationshipStatus(Integer relationshipStatus) { this.relationshipStatus = relationshipStatus; }
+	}
+
+	public enum UserEvent {
+		BIRTHDAY(1);
+		private Integer userEvent;
+		private UserEvent(Integer userEvent){ this.userEvent = userEvent; }
+		public Integer getUserEvent() { return userEvent; }
+		public void setUserEvent(Integer userEvent) { this.userEvent = userEvent; }
+	}
+
+	public enum EducationStatus {
+		HIGH_SCHOOL(1), UNDERGRAD(2), ALUM(3) ;
+		private Integer educationStatus;
+		private EducationStatus(Integer educationStatus){ this.educationStatus = educationStatus; }
+		public Integer getEducationStatus() { return educationStatus; }
+		public void setEducationStatus(Integer educationStatus) { this.educationStatus = educationStatus; }
+	}
+		
+
 }
