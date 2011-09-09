ba3e8f67f1066f9c12027bf319ee415c84f23ad6
Will Taylor <will.taylor@dealer.com>
2011-09-09 17:31:50 -0400

    not complete

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/api/AdOperations.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/AdOperations.java
@@ -1,7 +1,13 @@
 package org.springframework.social.facebook.api;
 
+import java.util.List;
+
 public interface AdOperations {
 
     AdAccount getAdAccount(String adAccountId);
+
+    List<AdGroup> getAdGroups(String adAccountId);
+
+    AdGroup getAdGroup(String adGroupId);
 
 }
