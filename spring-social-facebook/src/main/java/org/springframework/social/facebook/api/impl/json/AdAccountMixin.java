377c313e1aad82b4fa8567c96060daf04481d55e
Will Taylor <will.taylor@dealer.com>
2011-09-09 09:37:23 -0400

    Finished AdAccount and added unit test

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/api/impl/json/AdAccountMixin.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/impl/json/AdAccountMixin.java
@@ -3,6 +3,7 @@ package org.springframework.social.facebook.api.impl.json;
 import java.util.List;
 
 import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.springframework.social.facebook.api.AdAccountGroup;
 import org.springframework.social.facebook.api.AdAccountUser;
