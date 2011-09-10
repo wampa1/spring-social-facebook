df1472f612df50fb525618a60f3509a041cb3e57
Will Taylor <will.taylor@dealer.com>
2011-09-10 10:40:22 -0400

    getAdGroups is working but needs better test data to fully test targeting

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/api/impl/json/AdAccountMixin.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/impl/json/AdAccountMixin.java
@@ -3,12 +3,11 @@ package org.springframework.social.facebook.api.impl.json;
 import java.util.List;
 
 import org.codehaus.jackson.annotate.JsonCreator;
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.springframework.social.facebook.api.AdAccountGroup;
 import org.springframework.social.facebook.api.AdAccountUser;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+//@JsonIgnoreProperties(ignoreUnknown = true)
 class AdAccountMixin {
 
     @JsonCreator
