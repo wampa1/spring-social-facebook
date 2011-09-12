f2a8ae778b8e9abf17beb3fc5c2c1eef312a89bf
Will Taylor <will.taylor@dealer.com>
2011-09-12 10:33:48 -0400

    ad creative first pass

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/api/AdCreative.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/AdCreative.java
@@ -1,5 +1,7 @@
 package org.springframework.social.facebook.api;
 
+import java.util.List;
+
 
 /**
  * @author Eric Mayhew
@@ -11,41 +13,42 @@ public class AdCreative {
 
 	private String name;
 	private String type;
-	private int objectId;
+	private long objectId;
 	private String body;
 	private String imageHash;
 	private String imageUrl;
-	private int creativeId;
+	private long creativeId;
 	private String countCurrentAdGroups; //?? A string?
 	private String title;
 	private String runStatus;
 	private String linkUrl;
 	private String previewUrl;
+	private String viewTag;
+	private List<String> altViewTags;
+	private String id;
+	private String urlTags;
 
 	
-	public AdCreative() {
-		super();
-	}
-	
-	public AdCreative(String name, String type, int objectId, String body,
-			String imageHash, String imageUrl, int creativeId,
-			String countCurrentAdGroups, String title, String runStatus,
-			String linkUrl, String previewUrl) {
-		super();
-		this.name = name;
-		this.type = type;
-		this.objectId = objectId;
-		this.body = body;
-		this.imageHash = imageHash;
-		this.imageUrl = imageUrl;
-		this.creativeId = creativeId;
-		this.countCurrentAdGroups = countCurrentAdGroups;
-		this.title = title;
-		this.runStatus = runStatus;
-		this.linkUrl = linkUrl;
-		this.previewUrl = previewUrl;
-	}
-	public String getName() {
+	public AdCreative(String name, String type, long objectId, String body, String imageHash, String imageUrl, long creativeId, String countCurrentAdGroups, String title, String runStatus, String linkUrl, String previewUrl, String viewTag, List<String> altViewTags, String id, String urlTags) {
+        this.name = name;
+        this.type = type;
+        this.objectId = objectId;
+        this.body = body;
+        this.imageHash = imageHash;
+        this.imageUrl = imageUrl;
+        this.creativeId = creativeId;
+        this.countCurrentAdGroups = countCurrentAdGroups;
+        this.title = title;
+        this.runStatus = runStatus;
+        this.linkUrl = linkUrl;
+        this.previewUrl = previewUrl;
+        this.viewTag = viewTag;
+        this.altViewTags = altViewTags;
+        this.id = id;
+        this.urlTags = urlTags;
+    }
+
+    public String getName() {
 		return name;
 	}
 	public void setName(String name) {
@@ -57,10 +60,10 @@ public class AdCreative {
 	public void setType(String type) {
 		this.type = type;
 	}
-	public int getObjectId() {
+	public long getObjectId() {
 		return objectId;
 	}
-	public void setObjectId(int objectId) {
+	public void setObjectId(long objectId) {
 		this.objectId = objectId;
 	}
 	public String getBody() {
@@ -81,10 +84,10 @@ public class AdCreative {
 	public void setImageUrl(String imageUrl) {
 		this.imageUrl = imageUrl;
 	}
-	public int getCreativeId() {
+	public long getCreativeId() {
 		return creativeId;
 	}
-	public void setCreativeId(int creativeId) {
+	public void setCreativeId(long creativeId) {
 		this.creativeId = creativeId;
 	}
 	public String getCountCurrentAdGroups() {
@@ -117,7 +120,37 @@ public class AdCreative {
 	public void setPreviewUrl(String previewUrl) {
 		this.previewUrl = previewUrl;
 	}
-	
-	
+
+    public String getViewTag() {
+        return viewTag;
+    }
+
+    public void setViewTag(String viewTag) {
+        this.viewTag = viewTag;
+    }
+
+    public List<String> getAltViewTags() {
+        return altViewTags;
+    }
+
+    public void setAltViewTags(List<String> altViewTags) {
+        this.altViewTags = altViewTags;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrlTags() {
+        return urlTags;
+    }
+
+    public void setUrlTags(String urlTags) {
+        this.urlTags = urlTags;
+    }
 	
 }
---
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/impl/json/AdCreativeMixin.java
@@ -0,0 +1,29 @@
+package org.springframework.social.facebook.api.impl.json;
+
+import java.util.List;
+
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+//@JsonIgnoreProperties(ignoreUnknown = true)
+class AdCreativeMixin {
+
+    @JsonCreator
+    AdCreativeMixin(@JsonProperty("name") String name,
+                    @JsonProperty("type") String type,
+                    @JsonProperty("object_id") long objectId,
+                    @JsonProperty("body") String body,
+                    @JsonProperty("image_hash") String imageHash,
+                    @JsonProperty("image_url") String imageUrl,
+                    @JsonProperty("creative_id") long creativeId,
+                    @JsonProperty("count_current_adgroups") String countCurrentAdGroups,
+                    @JsonProperty("title") String title,
+                    @JsonProperty("run_status") String runStatus,
+                    @JsonProperty("link_url") String linkUrl,
+                    @JsonProperty("preview_url") String previewUrl,
+                    @JsonProperty("view_tag") String viewTag,
+                    @JsonProperty("alt_view_tags") List<String> altViewTags,
+                    @JsonProperty("id") String id,
+                    @JsonProperty("url_tags") String urlTags) {}
+
+}
