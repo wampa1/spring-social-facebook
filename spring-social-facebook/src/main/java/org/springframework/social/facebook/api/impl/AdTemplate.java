80b924136249eb5c01af4db02629cb897652fbe7
Will Taylor <will.taylor@dealer.com>
2011-09-12 11:19:23 -0400

    adding the rest of the get operations

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/api/impl/AdTemplate.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/impl/AdTemplate.java
@@ -3,6 +3,8 @@ package org.springframework.social.facebook.api.impl;
 import java.util.List;
 
 import org.springframework.social.facebook.api.AdAccount;
+import org.springframework.social.facebook.api.AdAccountGroup;
+import org.springframework.social.facebook.api.AdCampaign;
 import org.springframework.social.facebook.api.AdCreative;
 import org.springframework.social.facebook.api.AdGroup;
 import org.springframework.social.facebook.api.AdOperations;
@@ -17,6 +19,21 @@ public class AdTemplate extends AbstractFacebookOperations implements AdOperatio
     public AdTemplate(GraphApi graphApi, boolean isAuthorized) {
         super(isAuthorized);
         this.graphApi = graphApi;
+    }
+
+    @Override
+    public List<AdAccountGroup> getAdAccountGroupsForUser(String userId) {
+        return graphApi.fetchConnections(userId, "adaccountgroups", AdAccountGroup.class);
+    }
+
+    @Override
+    public AdAccountGroup getAdAccountGroup(String adAccountGroupId) {
+        return graphApi.fetchObject(adAccountGroupId, AdAccountGroup.class);
+    }
+
+    @Override
+    public List<AdAccount> getAdAccountsForUser(String userId) {
+        return graphApi.fetchConnections(userId, "adaccounts", AdAccount.class);
     }
 
     @Override
@@ -42,6 +59,21 @@ public class AdTemplate extends AbstractFacebookOperations implements AdOperatio
     @Override
     public AdCreative getCreative(String creativeId) {
         return graphApi.fetchObject(creativeId, AdCreative.class);
+    }
+
+    @Override
+    public List<AdCampaign> getAdCampaigns(String adAccountId) {
+        return graphApi.fetchConnections(ACT_PREFIX + adAccountId, "adcampaigns", AdCampaign.class);
+    }
+
+    @Override
+    public AdCampaign getAdCampaign(String campaignId) {
+        return graphApi.fetchObject(campaignId, AdCampaign.class);
+    }
+
+    @Override
+    public List<AdGroup> getAdGroupsForCampaign(String campaignId) {
+        return graphApi.fetchConnections(campaignId, "adgroups", AdGroup.class);
     }
 
 }
