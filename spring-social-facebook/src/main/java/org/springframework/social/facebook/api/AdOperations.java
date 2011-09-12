eaf35bf32ff091e56fcc494a5fce1bf994ad0c2b
Will Taylor <will.taylor@dealer.com>
2011-09-12 11:19:23 -0400

    adding the rest of the get operations

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/api/AdOperations.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/api/AdOperations.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public interface AdOperations {
 
+    List<AdAccountGroup> getAdAccountGroupsForUser(String userId);
+
+    AdAccountGroup getAdAccountGroup(String adAccountGroupId);
+
+    List<AdAccount> getAdAccountsForUser(String userId);
+
     AdAccount getAdAccount(String adAccountId);
 
     List<AdGroup> getAdGroups(String adAccountId);
@@ -13,5 +19,11 @@ public interface AdOperations {
     List<AdCreative> getCreatives(String adAccountId);
 
     AdCreative getCreative(String creativeId);
+
+    List<AdCampaign> getAdCampaigns(String adAccountId);
+
+    AdCampaign getAdCampaign(String campaignId);
+
+    List<AdGroup> getAdGroupsForCampaign(String campaignId);
 
 }
